920c170daec3d6ba2095bcef0dcd3c2829058577
Dan VerWeire <dverweire@gmail.com>
2018-11-13 09:53:29 -0500

    add res.json(), res.xml(), res.tablify(), res.xlsx()

--- test.js
+++ test.js
@@ -4,8 +4,32 @@ var platform = require('./')
 
 var app = platform()
 
+var obj = {
+	test : true
+	, string : 'String'
+	, number : 123456
+	, float : 123456.789
+	, boolean : true
+}
+
 app.get('/health-check', function (req, res, next) {
 	res.end('OK');
+});
+
+app.get('/json', function (req, res, next) {
+	res.json(obj);
+});
+
+app.get('/xlsx', function (req, res, next) {
+	res.xlsx(obj);
+});
+
+app.get('/xml', function (req, res, next) {
+	res.xml(obj);
+});
+
+app.get('/tablify', function (req, res, next) {
+	res.tablify(obj);
 });
 
 app.use(platform._404());
