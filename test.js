516a13e68299c5db839648e05308cd1b1af772bf
Dan VerWeire <dverweire@gmail.com>
2021-05-20 13:36:04 -0400

    update test.js with async cases

--- test.js
+++ test.js
@@ -13,50 +13,60 @@ var obj = {
 	, boolean : true
 };
 
-app.get('/health-check', function (req, res, next) {
+app.get('/health-check', function a (req, res, next) {
 	res.end('OK');
 });
 
-app.get('/json', function (req, res, next) {
+app.get('/json', function b (req, res, next) {
 	res.json(obj);
 });
 
-app.get('/xlsx', function (req, res, next) {
+app.get('/xlsx', function c (req, res, next) {
 	res.xlsx(obj);
 });
 
-app.get('/send-buffer', function (req, res, next) {
+app.get('/send-buffer', function d (req, res, next) {
 	res.send(Buffer.from('hello there buffer'));
 });
 
-app.get('/send-json', function (req, res, next) {
+app.get('/send-json', function e (req, res, next) {
 	res.send([{ hello : 'wolrd' }]);
 });
 
-app.get('/send-html', function (req, res, next) {
+app.get('/send-html', function f (req, res, next) {
 	res.send('<div style="color:blue">hello!</div>');
 });
 
-app.get('/send-file', function (req, res, next) {
+app.get('/send-file', function g (req, res, next) {
 	res.sendFile('./package.json');
 });
 
-app.get('/send-file-not-found', function (req, res, next) {
+app.get('/send-file-not-found', function h (req, res, next) {
 	res.sendFile('./asdfasdfasdfasdf', function (err) {
 		res.status(404).send(err);
 	});
 });
 
-app.get('/send-file-not-found-no-callback', function (req, res, next) {
+app.get('/send-file-not-found-no-callback', function i (req, res, next) {
 	res.sendFile('./asdfasdfasdfasdf');
 });
 
-app.get('/xml', function (req, res, next) {
-	res.xml({ result : obj });
+app.get('/xml', function j (req, res, next) {
+	res.xml(obj);
 });
 
-app.get('/tablify', function (req, res, next) {
+app.get('/tablify', function k (req, res, next) {
 	res.tablify(obj);
+});
+
+app.get('/async', async function l (req, res) {
+	//this will automatically call next
+	console.log('here1')
+});
+
+app.get('/async', async function m (req, res, next) {
+	//this will not resolve by itself, next must be called
+	console.log('here2');
 });
 
 app.mount('/test', app2);
